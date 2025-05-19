70aad1b6234651f6f4f054446d78546ad236822a
Roy Cloud 9 <roymail@ejemplo.com>
2025-05-19 01:45:54 +0000

    PRT-1

--- HU1.py
+++ HU1.py
@@ -1,1 +1,24 @@
-#esta es la historia de usuario 1
+#Escribe un programa que pida al usuario una palabra e imprima cuántas vocales contiene.
+#Requisitos:
+#Usar un ciclo for para recorrer la palabra.
+#Usar if para verificar si cada letra es una vocal.
+#Ejemplo:
+#Entrada: "computadora"
+#Salida: "La palabra tiene 5 vocales."
+
+def contar_vocales(palabra):
+    vocales = "aeiouAEIOU"
+    contador = 0
+
+    #palabra=gonzalo
+    for letra in palabra:
+        #letra=o
+        if letra in vocales:
+          #buscar o, si es que esta dentro del arreglo vocales
+            contador += 1
+
+    return contador
+
+palabra_ingresada = input("Ingrese una palabra: ")
+cantidad_vocales = contar_vocales(palabra_ingresada)
+print(f"La palabra tiene {cantidad_vocales} vocales.")
